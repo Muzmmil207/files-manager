c5851f2f5fe6e22ba9d5292ad7ca812bbc874b78
mozmmil-ali <mly88207@gmail.com>
2023-03-08 15:16:37 +0200

    -

--- core/base.py
+++ core/base.py
@@ -1,22 +1,33 @@
+from datetime import datetime
+
+
 class File:
-    def __init__(self, name, content=[]):
+    def __init__(self, name=None, content=[]):
         self.name = name
         self.content = content
 
     def commit(self):
-        with open(f'files/text/{self.name}.txt', 'w') as file:
-            content = ''.join(self.content)
+        with open(self.path, "w") as file:
+            self.content.append("\n\n\n{0:%Y-%m-%d %I:%M%p}".format(datetime.now()))
+            content = "".join(self.content)
             file.write(content)
-        pass
+            file.close()
 
     def read(self):
-        pass
+        with open(self.path, "r") as file:
+            print(file.read())
+            file.close()
 
     def uniq_together(self):
         pass
 
+    @property
+    def path(self):
+        return "files/{0}/{1}.{0}".format(self.format, self.name)
+
 
 class TextFile(File):
+    format = "txt"
+
     def __init__(self, name, content):
-        self._format = ".txt"
         super().__init__(name, content)
--- core/manager.py
+++ core/manager.py
@@ -1,44 +1,64 @@
 from base import TextFile
-
+import sys
 
 class FileManager:
     files_types = {"text": TextFile}
-    
-    def __init__(self) -> None:
+
+    def __init__(self):
         self._files_data = []
 
     def search_files(self, format=None):
-        pass
+        self.run()
 
     def display_all_files(self):
         for file in self.files_data:
             print(file)
+            self.run()
 
     def new_file(self):
         print("For save type SAVE() for close CLOSE()")
-        print("================")
+        print("================================================")
         content = []
         format = input("chose save format ({}) ".format(", ".join(self.files_types.keys())))
-        while line := input():
+        while line := input("_ "):
             if line == "SAVE()":
-                file_name = input('Enter a name or lave it empty for default\n:')
-                new = self.files_types[format](name=file_name,content=content)
+                file_name = input("Enter a name or lave it empty for default: ")
+                new = self.files_types[format](name=file_name, content=content)
                 new.commit()
+                new.read()
                 self.files_data.append(new)
+                input()
                 break
             elif line == "CLOSE()":
-                self.run()
                 break
-            content.append(line + "\n")
 
-    def save_file():
-        return
+            content.append(line + "\n")
+        self.run()
+
+    def run(self):
+        print(
+            "for new file => n\nfor displaying all files => d\nfor search files => s\nfor exit => e"
+        )
+        print("================================================")
+        option = input('> ').lower()
+        if option == 'n':
+            self.new_file()
+        elif option == 'd':
+            self.display_all_files()
+        elif option == 's':
+            self.search_files()
+        elif option == 'e':
+            sys.exit()
+        else:
+            self.run()
+
 
     @property
     def files_data(self):
-        if not self._files_data:
+        if not self._files_data.__len__() == 0:
             print("Retrieving The Data...")
             self._files_data = []
         return self._files_data
 
-FileManager().new_file()
+if __name__ == '__main__':
+    FileManager().run()
